3efad9a9052195c727bf38369e542168b59f062e
hchiam <hchiam@users.noreply.github.com>
2022-02-17 21:25:40 -0500

    working accessible progress bar storybook demo

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,55 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  return (
+    <SizedProgressBar
+      role="progressbar"
+      aria-valuenow={value}
+      aria-valuemin="0"
+      aria-valuemax="100"
+      size={size}
+      value={value}
+    >
+      <Bar value={value}></Bar>
+    </SizedProgressBar>
+  );
+};
+
+const SizedProgressBar = styled.div`
+  height: ${(p) => p.size};
+  padding: ${(p) => (p.size === ProgressBarSizes.LARGE ? "4px" : null)};
+  background: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  border-radius: 8px;
+  width: 370px;
+  height: ${(p) => p.size};
+  overflow: hidden;
+`;
+
+const Bar = styled.div`
+  display: block;
+  width: 100%;
+  height: 100%;
+  background: transparent;
+  clip-path: inset(0px 0px 0px 0px round 4px);
+  :before {
+    content: "";
+    display: block;
+    width: ${(p) => `${p.value}%`};
+    height: 100%;
+    background: ${COLORS.primary};
+  }
+`;
+
+export const ProgressBarSizes = {
+  LARGE: "24px",
+  MEDIUM: "12px",
+  SMALL: "8px",
 };
 
 export default ProgressBar;
