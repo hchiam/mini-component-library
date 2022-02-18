8270304d29e83faf3fef70930b5a959ca0707fb0
hchiam <hchiam@users.noreply.github.com>
2022-02-17 23:08:04 -0500

    better perf. more cohesive styles in one spot.

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,7 +5,30 @@ import styled from "styled-components";
 import { COLORS } from "../../constants";
 import VisuallyHidden from "../VisuallyHidden";
 
+export const ProgressBarSizes = {
+  LARGE: "24px",
+  MEDIUM: "12px",
+  SMALL: "8px",
+};
+
+const STYLES = {
+  [ProgressBarSizes.LARGE]: {
+    height: "24px",
+    padding: "4px",
+  },
+  [ProgressBarSizes.MEDIUM]: {
+    height: "12px",
+    padding: 0,
+  },
+  [ProgressBarSizes.SMALL]: {
+    height: "8px",
+    padding: 0,
+  },
+};
+
 const ProgressBar = ({ value, size }) => {
+  const styles = STYLES[handleSize(size)];
+  if (!styles) throw new Error(`Unknown size passed to ProgressBar: ${size}`);
   return (
     <SizedProgressBar
       role="progressbar"
@@ -13,22 +36,26 @@ const ProgressBar = ({ value, size }) => {
       aria-valuemin="0"
       aria-valuemax="100"
       size={size}
+      style={{
+        "--height": styles.height,
+        "--padding": styles.padding,
+      }}
     >
-      <Bar value={value}></Bar>
+      <VisuallyHidden>{value}%</VisuallyHidden>
+      <Bar style={{ "--width": value + "%" }}></Bar>
     </SizedProgressBar>
   );
 };
 
 const SizedProgressBar = styled.div`
-  height: ${(p) => handleSize(p.size)};
-  padding: ${(p) =>
-    handleSize(p.size) === ProgressBarSizes.LARGE ? "4px" : null};
+  height: var(--height);
+  padding: var(--padding);
   background: ${COLORS.transparentGray15};
   box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
   border-radius: 8px;
   width: 370px;
   height: ${(p) => handleSize(p.size)};
-  overflow: hidden;
+  overflow: hidden; /* to trim end corners when near end of progress bar */
 `;
 
 function handleSize(size) {
@@ -48,16 +75,10 @@ const Bar = styled.div`
   :before {
     content: "";
     display: block;
-    width: ${(p) => `${p.value}%`};
+    width: var(--width); /* for better perf, make this a CSS variable */
     height: 100%;
     background: ${COLORS.primary};
   }
 `;
-
-export const ProgressBarSizes = {
-  LARGE: "24px",
-  MEDIUM: "12px",
-  SMALL: "8px",
-};
 
 export default ProgressBar;
