0ba82b1f1dc7b690ab8f614e79c84b1752ca8af3
hchiam <hchiam@users.noreply.github.com>
2022-02-17 23:13:25 -0500

    head start on Select.js

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
     <select value={value} onChange={onChange}>
-      {children}
+      {children} <Icon id="chevron-down" size={12}></Icon>
     </select>
   );
 };
